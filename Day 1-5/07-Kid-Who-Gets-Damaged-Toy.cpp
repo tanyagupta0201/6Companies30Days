ca25f0b328ae4f6d6e543d378c34692d101de582
Tanya Gupta <tanyagupta.pg@gmail.com>
2022-01-01 09:08:30 +0530

    Added Find kid who gets damaged toy

---
+++ Day 1-5/07-Kid-Who-Gets-Damaged-Toy.cpp
@@ -0,0 +1,38 @@
+// Problem Link: https://www.geeksforgeeks.org/distributing-m-items-circle-size-n-starting-k-th-position/
+
+// Time Complexity: O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int FindPosition(int a, int b, int c)
+{
+    int n;
+
+    n = (a + c - 1) % b;
+
+    if (n == 0)
+    {
+        return b;
+    }
+    return n;
+}
+
+int main()
+{
+    // a -> number of toys
+    // b -> number of kids
+    // c -> starting position
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << "The kid who gets the damaged toy is at position: " << FindPosition(a, b, c) << endl;
+    return 0;
+}
+
+/*
+    Output
+
+    8 5 2
+    The kid who gets the damaged toy is at position: 4
+*/
