9b17b987fbee1d33681e7bfd890538cbc23adcc0
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-01-15 12:23:36 +0530

    Update 14-Minimum-Steps-To-Destination.cpp

--- Day 11-15/14-Minimum-Steps-To-Destination.cpp
+++ Day 11-15/14-Minimum-Steps-To-Destination.cpp
@@ -1,5 +1,10 @@
 // Problem Link: https://practice.geeksforgeeks.org/problems/minimum-number-of-steps-to-reach-a-given-number5234/1/
 
+/* 
+    1. Add steps till we reach target.
+    2. If the target is surpassed: Add steps until we can subtract 2 * K steps to subtract that makes us reach target ((sum - target) % 2 == 0)
+*/
+
 class Solution
 {
 public:
